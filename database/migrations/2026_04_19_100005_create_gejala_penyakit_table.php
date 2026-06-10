9ac2fdfe76ddf3751c8f074d0627435f83ede8e9
Bright Eliezer Pinaria <brightpinaria@student.telkomuniversity.ac.id>
2026-06-10 13:42:17 +0700

    solve error
    s

--- database/migrations/2026_04_19_100005_create_gejala_penyakit_table.php
+++ database/migrations/2026_04_19_100005_create_gejala_penyakit_table.php
@@ -15,12 +15,10 @@ class CreateGejalaPenyakitTable extends Migration
     {
         Schema::create('gejala_penyakit', function (Blueprint $table) {
             $table->id();
-            $table->integer('gejala_id')->unsigned();
-            $table->integer('fasilitas_kesehatan_id')->unsigned();
+            $table->foreignId('gejala_id')->constrained('gejalas')->cascadeOnDelete();
+            $table->foreignId('fasilitas_kesehatan_id')->constrained('fasilitas_kesehatan')->cascadeOnDelete();
             $table->float('value_cf')->nullable();
-            
-            $table->foreign('gejala_id')->references('id')->on('gejalas')->onDelete('cascade');
-            $table->foreign('fasilitas_kesehatan_id')->references('id')->on('fasilitas_kesehatans')->onDelete('cascade');
+        });
         });
     }
 
